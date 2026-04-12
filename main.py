28c62e40980c44f1936f60b62a37863564cd3830
k4runa <3nesslbr@gmail.com>
2026-04-12 20:41:19 +0300

    add other endpoints

--- main.py
+++ main.py
@@ -11,9 +11,20 @@ API_KEY = os.getenv("API_KEY")
 os.makedirs(os.path.dirname(str(DB_PATH)),exist_ok=True)
 app = FastAPI()
 
-users = UserManager(db_path=DB_PATH,echo=False) #type:ignore
-
+users = UserManager(db_path=DB_PATH,echo=False)     #type:ignore
+movies = MovieManager(db_path=DB_PATH,echo=False)   #type:ignore
 @app.post("/users")
 def register(user:UserScheme):
     users.add_user(user) #type:ignore
     return {"success"}
+
+@app.post("/movies/{username}")
+def add_movie(username: str, movie: MovieScheme):
+    movies.add_movie(username=username, query=movie.query)  #type: ignore
+    return {"success": True}
+
+@app.get("/movies/recommendations/{username}")
+def get_recommendations(username: str):
+    top_genres = movies.get_top_genres(username)
+    recommendations = fetch_recommendations(top_genres)
+    return {"recommendations": recommendations}
