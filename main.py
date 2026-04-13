192b8275d3130b43b621ccb935bc37616604a6a7
Enes <3nesslbr@gmail.com>
2026-04-13 13:38:16 +0300

    decorator and get_all_users api

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from services.database import UserManager,MovieManager,MovieScheme,UserScheme,logger,UserAlreadyExists,UserNotFoundError,MovieAlreadyExists
+from services.database import (
+    UserManager,
+    MovieManager,
+    MovieScheme,
+    UserScheme,
+    logger,
+    UserAlreadyExists,
+    UserNotFoundError,
+    MovieAlreadyExists,
+)
 from services.tmdb import fetch_recommendations
-from fastapi import FastAPI,Request,HTTPException
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
-import os 
+import os
 from dotenv import load_dotenv
+from functools import wraps
+
 load_dotenv()
 
 DB_PATH = os.getenv("DB_PATH")
@@ -21,52 +32,77 @@ if not API_KEY:
     logger.warning("API_KEY not set")
 app = FastAPI()
 
-users = UserManager(db_path=DB_PATH,echo=False)     #type:ignore
-movies = MovieManager(db_path=DB_PATH,echo=False)   #type:ignore
+users = UserManager(db_path=DB_PATH, echo=False)  # type:ignore
+movies = MovieManager(db_path=DB_PATH, echo=False)  # type:ignore
+
 
 @app.exception_handler(RequestValidationError)
 async def validation_handler(request: Request, exc: RequestValidationError):
     logger.info(f"{request.method} {request.url} -> {exc}")
-    return JSONResponse(
-        status_code=422,
-        content={"detail": exc.errors()}
-    )
+    return JSONResponse(status_code=422, content={"detail": exc.errors()})
+
 
 @app.exception_handler(UserNotFoundError)
 async def user_not_found_handler(request: Request, exc: UserNotFoundError):
     logger.warning(f"{request.method} {request.url} -> {exc}")
     return JSONResponse(status_code=404, content={"detail": "User not found"})
 
+
 @app.exception_handler(UserAlreadyExists)
 async def user_exists_handler(request: Request, exc: UserAlreadyExists):
     logger.warning(f"{request.method} {request.url} -> {exc}")
     return JSONResponse(status_code=409, content={"detail": "User already exists."})
+
 
 @app.exception_handler(MovieAlreadyExists)
 async def movie_exists_handler(request: Request, exc: MovieAlreadyExists):
     logger.warning(f"{request.method} {request.url} -> {exc}")
     return JSONResponse(status_code=409, content={"detail": "Movie already exists"})
 
+
 @app.exception_handler(Exception)
 async def global_exception_handler(request: Request, exc: Exception):
     logger.exception(f"{request.method} {request.url} -> {exc}")
-    return JSONResponse(
-        status_code=500,
-        content={"detail": "Internal server error"}
-    )
+    return JSONResponse(status_code=500, content={"detail": "Internal server error"})
 
+
+def print_log(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        logger.info(f"running function: {func.__name__}")
+        result = func(*args, **kwargs)
+        logger.info(f"done function: {func.__name__}")
+        return result
+
+    return wrapper
+
+
+@print_log
 @app.post("/users")
-def register(user:UserScheme):
-    users.add_user(user) #type:ignore
-    return {"success": True ,"message": {"User successfully added."}}
+def register(user: UserScheme):
+    users.add_user(user)  # type:ignore
+    return {"success": True, "message": "User successfully added."}
 
+
+@print_log
 @app.post("/movies/{username}")
 def add_movie(username: str, movie: MovieScheme):
-    movies.add_movie(username=username, query=movie.query)  #type: ignore
+    movies.add_movie(username=username, query=movie.query)  # type: ignore
     return {"success": True, "message": "Movie successfully added."}
 
+
+@print_log
 @app.get("/movies/recommendations/{username}")
 def get_recommendations(username: str):
     top_genres = movies.get_top_genres(username)
     recommendations = fetch_recommendations(top_genres)
     return {"success": True, "data": {"recommendations": recommendations}}
+
+
+@print_log
+@app.get("/users/get_all_users")
+def get_all_users():
+    logger.info("Fetching all users from database...")
+    all_users = users.get_all_users()
+    logger.info("Fetched all users")
+    return {"success": True, "data": {"users": all_users}}
