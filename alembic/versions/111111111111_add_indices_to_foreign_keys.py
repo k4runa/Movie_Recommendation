203ac0de45505b0502b870f18a10d2dc2bfc5bd2
k4runa <3nesslbr@gmail.com>
2026-04-18 21:23:49 +0300

    fix: resolve alembic multiple heads error

--- alembic/versions/111111111111_add_indices_to_foreign_keys.py
+++ alembic/versions/111111111111_add_indices_to_foreign_keys.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "111111111111"
-down_revision: Union[str, Sequence[str], None] = "f1583581fded"
+down_revision: Union[str, Sequence[str], None] = "398019c7597c"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
